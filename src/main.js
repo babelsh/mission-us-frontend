ccdadd962c1ebdde1a9826df28581b27c20f7fb2
rartner <ramon.artner@gmail.com>
2018-09-15 16:07:52 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
+import '@babel/polyfill';
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-
-Vue.config.productionTip = false;
 
 new Vue({
   router,
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify, {
+  theme: {
+    error: '#f66',
+    primary: '#fd9051',
+    secondary: '#976dd1',
+    success: '#77d353',
+  },
+});
