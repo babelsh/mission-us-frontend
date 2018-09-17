814338e41c7886426d530eba5e9a8210751da2b9
rartner <ramon.artner@gmail.com>
2018-09-17 17:50:54 -0300

    removing dist from gitignore

---
+++ dist/precache-manifest.b419d4cbb1237312408d2f3b7501f1ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "50e14d7658070660ed73",
+    "url": "/js/chunk-vendors.3f528f40.js"
+  },
+  {
+    "revision": "6e5e37276f792005a908",
+    "url": "/js/app.72c59d8a.js"
+  },
+  {
+    "revision": "6249ec0a09fa811f3d25",
+    "url": "/js/about.2ca2c031.js"
+  },
+  {
+    "revision": "62e75f6496cd2cb91aafc7edf26d6a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e055714d2c647827907bbf144cf409f",
+    "url": "/img/logo.5e055714.png"
+  },
+  {
+    "revision": "2073af90dfccf25ba6550d7d3070bdd8",
+    "url": "/img/logo.2073af90.png"
+  },
+  {
+    "revision": "50e14d7658070660ed73",
+    "url": "/css/chunk-vendors.0c3e5376.css"
+  },
+  {
+    "revision": "6e5e37276f792005a908",
+    "url": "/css/app.3a5a9620.css"
+  }
+];
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
 
-const app = express()
-app.use(history());
-
-console.log('diirrr name ', __dirname);
+const app = express();
 app.use(serveStatic(`${__dirname}/dist`));
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started ', path);
+
+console.log('Server running...', path);
