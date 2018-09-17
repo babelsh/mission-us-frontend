ebd06b3e3e7b8d2368d8bc56c00504208343f243
rartner <ramon.artner@gmail.com>
2018-09-17 15:13:02 -0300

    creating activity component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-navigation-drawer
       persistent
+      clipped
       v-model="drawer"
       enable-resize-watcher
       fixed
@@ -23,7 +24,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar app class="primary">
+    <v-toolbar app clipped-left class="primary">
       <v-toolbar-side-icon
         class="white--text"
         @click.stop="drawer = !drawer"
---
+++ src/components/ActivityCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card>
+    <v-img
+      :height="imageHeight"
+      :src="imgSrc"
+    ></v-img>
+    <v-card-title>
+      <div>
+        <span class="title">{{ title }}</span>
+      </div>
+    </v-card-title>
+    <v-card-actions style="padding-top: 0px">
+      <v-btn flat>
+        Saber mais
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="secondary">
+        Feito
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'activity-card',
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    imageHeight() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return '150px';
+        case 'sm': return '200px';
+        case 'md': return '200px';
+        case 'lg': return '200px';
+        case 'xl': return '200px';
+        default: console.log('inválid screen size.');
+      }
+
+      return '';
+    },
+  },
+};
+</script>
