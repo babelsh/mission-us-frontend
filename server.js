d828102d4dd0caf95d3f245ed56f7e568dfc81bb
rartner <ramon.artner@gmail.com>
2018-09-17 17:45:01 -0300

    correcting start script for heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
-const app = express();
+const app = express()
+app.use(history());
+
+console.log('diirrr name ', __dirname);
 app.use(serveStatic(`${__dirname}/dist`));
-
 const port = process.env.PORT || 5000;
-app.listen(port);
 
-console.log('Server running...\n', path);
+app.listen(port);
+console.log('server started ', path);
