dd07e952c3c8b5f09536c83e4fa8b5d97c84048f
rartner <ramon.artner@gmail.com>
2018-09-18 20:32:51 -0300

    updating icons

--- vue.config.js
+++ vue.config.js
@@ -7,11 +7,11 @@ module.exports = {
     themeColor: '#fd9051',
     msTileColor: '#FFF',
     iconPaths: {
-      favicon16: '/public/img/icons/favicon-16x16.png',
-      favicon32: '/public/img/icons/favicon-32x32.png',
-      appleTouchIcon: '/public/img/icons/apple-touch-icon-152x152.png',
-      maskIcon: '/public/img/icons/safari-pinned-tab.svg',
-      msTileImage: '/public/img/icons/msapplication-icon-144x144.png.png',
+      favicon16: 'public/img/icons/favicon-16x16.png',
+      favicon32: 'public/img/icons/favicon-32x32.png',
+      appleTouchIcon: 'public/img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'public/img/icons/safari-pinned-tab.svg',
+      msTileImage: 'public/img/icons/msapplication-icon-144x144.png.png',
     },
   },
 };
