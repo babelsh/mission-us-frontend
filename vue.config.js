99317b4709b8d49ea93026270023092205642c90
rartner <ramon.artner@gmail.com>
2018-09-18 18:54:37 -0300

    adding proper icons to web/mobile app

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,12 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'black',
     themeColor: '#fd9051',
     msTileColor: '#FFF',
+    iconPaths: {
+      favicon16: '/public/img/icons/favicon-16x16.png',
+      favicon32: '/public/img/icons/favicon-32x32.png',
+      appleTouchIcon: '/public/img/icons/apple-touch-icon-152x152.png',
+      maskIcon: '/public/img/icons/safari-pinned-tab.svg',
+      msTileImage: '/public/img/icons/msapplication-icon-144x144.png.png',
+    },
   },
 };
