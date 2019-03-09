55b0313430b51781d518619df46555d816c0e6c6
Ismael Costa <ismael.antiqueira@gmail.com>
2019-03-08 23:41:50 -0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,13 @@
             <v-card dark flat>
               <v-img
                 height="200"
-                src="https://www.navegg.com/nvgadm/wp-content/uploads/2016/12/navegg-161212-modelo-de-atribuicao-a-visao-holistica-da-jornada-de-compra-2-940x400.jpg"                
+                src="http://psicopedagogiacuritiba.com.br/wp-content/uploads/2016/05/o-livre-brincar-e-o-desempenho-escolar-1.jpg"                
                 gradient="to top, rgba(0,0,0,.44), rgba(0,0,0,.44)"
               >
                 <v-container fill-height>
                   <v-layout align-center>              
                     <v-layout column justify-end>
-                      <h3 class="headline mb-0 has-padding">Trilha: descobrindo seu filho</h3>
+                      <h3 class="headline mb-0 has-padding">Trilha: Brincar e o desenvolvimento da criança</h3>
                       <div class="mb-0 has-padding">Março, 2019</div>
                     </v-layout>
                   </v-layout>
@@ -29,12 +29,12 @@
               <v-flex>
                 <v-avatar>
                   <v-img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuWMekAs_Ldlhug3VGPGqv-0qSstNzs2SgxDCEMl79OAMMKbca"
+                    src="http://isabelateixeiradacosta.com.br/wp-content/uploads/brincar-207x300.jpg"
                   />
                 </v-avatar>
-               <strong>A Criança e seu Desenvolvimento</strong>
+               <strong>Importância do brincar e os brinquedos pedagógicos</strong>
                 <div class="caption">
-                  Ambiente, afeto, rotina: tudo isso exerce influência na evolução da criança. E os cuidados devem começar ainda na gravidez.
+                  Ambiente, afeto, rotina: tudo isso exerce influência na evolução da criança. E o brincar é melhor maneira para desenvolver a criança.
                 </div>
               </v-flex>
             </v-layout>
@@ -47,34 +47,17 @@
               <v-flex>
                 <v-avatar>
                   <v-img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNnhr8k4XVKfUomGttYdMbEVQafTcRkdyFMvI1YspFltIpoCCv"
+                  src="https://demonstre.com/wp-content/uploads/2018/01/espacial.jpg"
                   />
                 </v-avatar>
-                <strong>Afeto é fundamental</strong>
+                <strong>Vamos brincar!</strong>
                 <div class="caption mb-2">
-                  A maior parte das pessoas não relaciona as ligações afetivas ao desenvolvimento infantil.
-                  Apesar de reconhecer que o carinho dos adultos é importante e influencia o temperamento da criança,
-                  apenas 12% da população] considera o afeto importante para o desenvolvimento.
+                   A essência da infância está nos momentos que a criança tem para a exploração: o faz de conta, a brincadeira e o jogo. Vamos explorar estes momentos com uma brincadeira.
                 </div>
                   <v-btn v-if="isActive(2)" block color="blue" dark @click="goToAtividade">
                     iniciar
                   </v-btn>
                   <v-btn v-else block color="grey lighten-1" dark>bloqueado</v-btn>
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-  
-          <v-timeline-item color="grey">
-            <v-layout wrap pt-3>
-              <v-flex>
-                <v-avatar>
-                  <v-img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Sl_A75vqKCouBWwshmzdokwnBILnYctL-C4TCw-9XPCocFMyBg"                    
-                  />
-                </v-avatar>
-                <strong>&nbsp; &nbsp; Aprendizado a toda hora e em todo lugar</strong>
-                <div class="caption mb-2">ara muitos brasileiros, o período mais importante da infância se inicia juntamente com a formação das lembranças que a criança levará consigo até a fase adulta, ou seja, entre os 2 e os 4 anos. E a maioria da população não considera que atividades como andar, correr, falar, entre outras, também façam parte do aprendizado cognitivo.</div>                        
-                <v-btn block color="grey lighten-1" dark>bloqueado</v-btn>
               </v-flex>
             </v-layout>
           </v-timeline-item>  
